fd0a70284a867df076fb00718c6a7a93348b9b30
Swapnil Nawale <swapnilnawale18@gmail.com>
2021-08-28 18:46:46 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BooleanDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool result;
+            int x = 10;
+            result = (!(x == 10));
+            Console.WriteLine(result);
+        }
+    }
+}
